d8f5a848c555826a5d5179b610df75c692334ab0
paulQR <paulqr10@gmail.com>
2019-07-31 12:33:12 +0200

    1 file modified

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import{Component} from '@angular/core';
-
+import{ FormGroup, FromControl, Validators} from '@angular/forms'
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html', 

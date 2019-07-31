50e2f640f472dd41a9b6f60d5a7bac9d17c515ab
paulQR <paulqr10@gmail.com>
2019-07-31 09:53:28 +0200

    2 files modified

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -23,8 +23,19 @@ export class TemplateComponent {
     usuario: Object = {
       nombre: null,
       apellido: null,
-      correo: null
+      correo: null,
+      pais: "",
+      sexo: "Hombre"
     }
+
+    paises = [{
+      codigo: "CRI",
+      nombre: "Costa Rica"
+    },
+    {
+      codigo: "ESP",
+      nombre: "España"   
+    }]
 
     constructor(){}
 

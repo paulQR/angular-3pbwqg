beac17617e54d316def07817239c0589b2b4e322
paulQR <paulqr10@gmail.com>
2019-08-01 11:39:19 +0200

    2 files modified

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,7 @@
 import{Component} from '@angular/core';
 import{ FormGroup, FormControl, Validators, FormArray} from '@angular/forms'
+import{ Observable } from 'rxjs/Rx'
+
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html', 
@@ -41,18 +43,29 @@ export class DataComponent {
                 'nombre': new FormControl( '', [ 
                                               Validators.required,
                                               Validators.minLength(3) ]),
-                'apellido': new FormControl('', Validators.required )
+                'apellido': new FormControl('', [
+                                                  Validators.required,
+                                                  this.noHerrera
+                                                ] )
             }),
             'correo': new FormControl('',   [
                               Validators.required, 
                               Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")] ),
             'pasatiempos': new FormArray([
                 new FormControl('Correr', Validators.required)
-            ])
+            ]),
+            'username' : new FormControl('', Validators.required),
+            'password1' : new FormControl('', Validators.required),
+            'password2' : new FormControl()
            });
 
             // pasa valores por defecto
             //this.forma.setValue(this.usuario);
+
+            this.forma.controls['password2'].setValidators([
+                Validators.required,
+                this.noIgual.bind(this.forma)
+            ])
     }
 
     agregarPasatiempo(){
@@ -62,11 +75,52 @@ export class DataComponent {
         )
     }
 
+    noHerrera( control: FormControl):{[s:string]: boolean}{
+
+          // si la validacion esta fallando
+          if(control.value === "herrera"){
+              return{
+                 noherrera: true
+              }
+          }
+
+          // si esta bien
+          return null;
+    }
+
+    /*
+    noIgual( control: FormControl):{[s:string]: boolean}{
+          if(control.value !== this.forma.controls['password1'].value){
+              return{
+                 noiguales: true
+              }
+          }
+          return null;
+    }
+    */
+
+    noIgual( control: FormControl):any{
+          //console.log(this);
+          let forma:any = this;
+          if(control.value !== forma.controls['password1'].value){
+              return{
+                 noiguales: true
+              }
+          }
+          return null;
+          
+    }
+
+    existeUsuario( control: FormControl): Promise<any>|Observable<any>{
+
+    }    
+
     guardarCambios(){
       console.log(this.forma.value);
       console.log(this.forma);
 
       //this.forma.reset(this.usuario);
+      /*
       this.forma.reset({
           nombrecompleto: {
             nombre:"",
@@ -74,6 +128,7 @@ export class DataComponent {
           },
           correo:""
       });
+      */
 
       //this.forma.controls['correo'].setValue('Nuevo correo@asdasd.com');
     }
